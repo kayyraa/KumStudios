4f969f7705bbc76c9da18f1b34a20f36aff73898
kayyraa <zindhukayrabasbay@gmail.com>
2024-09-05 11:38:49 +0300

    Auto-update from local project folder

--- scripts/hud.js
+++ scripts/hud.js
@@ -1,21 +1,54 @@
-import { News } from "./news.js";
+import { Pages } from "./news.js";
 
 const Content = document.getElementById("Content");
 
-News.forEach(Page => {
+Pages.forEach(Page => {
+    const PageTimestamp = Page.Timestamp;
     const PageTitle = Page.Title;
     const PageContent = Page.Content;
+    const PageLongContent = Page.LongContent;
 
     const Viewpage = document.createElement("viewpage");
+    Viewpage.setAttribute("focused", "false");
     Content.appendChild(Viewpage);
 
     const TitleLabel = document.createElement("span");
     TitleLabel.innerHTML = PageTitle;
     Viewpage.appendChild(TitleLabel);
-    
+ 
     const ContentParagraph = document.createElement("p");
     ContentParagraph.innerHTML = PageContent;
     Viewpage.appendChild(ContentParagraph);
 
-    Viewpage.addEventListener("click", Page.Click);
+    const TimestampLabel = document.createElement("pt");
+    TimestampLabel.innerHTML = PageTimestamp;
+    Viewpage.appendChild(TimestampLabel);
+
+    const LongContentParagraph = document.createElement("pr");
+    LongContentParagraph.innerHTML = PageLongContent;
+    LongContentParagraph.style.opacity = "0";
+    LongContentParagraph.id = "LongContentParagraph";
+    Viewpage.appendChild(LongContentParagraph);
+
+    TitleLabel.addEventListener("click", Page.Click);
+
+    Viewpage.addEventListener("click", function() {
+        const Focused = this.getAttribute("focused") === "false";
+
+        this.style.position = Focused ? "absolute" : "relative";
+        this.style.height = Focused ? "100%" : "50%";
+        this.setAttribute("focused", Focused);
+        LongContentParagraph.style.opacity = Focused ? "1" : "0";
+        TimestampLabel.style.opacity = Focused ? "0" : "1";
+
+        Array.from(Content.getElementsByTagName("viewpage")).forEach(OtherViewpage => {
+            if (OtherViewpage !== this) {
+                OtherViewpage.setAttribute("focused", "false");
+                OtherViewpage.style.display = Focused ? "none" : "block";
+            }
+        }); 
+
+        const AnyFocused = Array.from(Content.getElementsByTagName("viewpage")).some(Viewpage => Viewpage.getAttribute("focused") === "true");
+        document.title = !AnyFocused ? "Küm Studios" : PageTitle;
+    });
 });
--- scripts/news.js
+++ scripts/news.js
@@ -1,7 +1,19 @@
-export const News = [
+export const Pages = [
     {
+        Timestamp: "4.09.2024",
         Title: "Official WhatsApp Group",
         Content: "Join to the official group for the official things that they are officially connected to official group.",
+        LongContent: "",
+
+        Click: () => {
+            window.location.href = "https://chat.whatsapp.com/DE2wpTUXHi4JR9IgqyASlO";
+        }
+    },
+    {
+        Timestamp: "5.09.2024",
+        Title: "Explosive diary story",
+        Content: "Look at that story its so great that you can learn more about this story and all of its details.",
+        LongContent: "One day, is my first time in this story and I will explain more about this story and all of its details.",
 
         Click: () => {
             window.location.href = "https://chat.whatsapp.com/DE2wpTUXHi4JR9IgqyASlO";
